a687882d7abf3f0d0d160440e57478e2e17406dc
trungtruc056 <trungtruc056@gmail.com>
2018-08-15 13:24:08 +0700

    Update View User Detail

--- src/Components/Users/UserDetail.js
+++ src/Components/Users/UserDetail.js
@@ -3,6 +3,8 @@ import UserInfo from './UserInfo';
 import UserPost from './UserPost';
 import UserAPI from './userAPI';
 import PostAPI from './../Posts/postAPI';
+import { Container, Row, Col } from 'reactstrap';
+import './Users.css';
 
 class UserDetail extends Component {
     constructor() {
@@ -26,32 +28,40 @@ class UserDetail extends Component {
             })
         })
     }
-
     render() {
         return (
-            <div className="user-detail">
-                <div className="user-info">
-                    {
-                        this.state.userDetail ?
-                            <UserInfo user={this.state.userDetail} />
-                        : 'User not found'
+            <Container>
+                    <Col md="12" className="mb-85">
+                        <div className="user-info">
+                            {
+                                this.state.userDetail ?
+                                    <UserInfo user={this.state.userDetail} />
+                                : 'User not found'
 
-                    }
-                </div>
-                <div className="user-post">
-                <h1>User Posts</h1>
-                    {
+                            }
+                        </div>
+                    </Col>
+                    
+                    <div className="user-post">
+                    {console.log(this.state.userDetail.userId)}
+                    <h1 className="text-center mb-30">Post of User</h1>
+                        {
+                            this.state.userPost ?
+                                <Row>
+                                {
+                                    this.state.userPost.map(item => (
+                                        <Col md="4" className="mb-30" key={item.id}>
+                                            <UserPost usp={item} />
+                                        </Col>
+                                    ))
+                                }
+                                </Row>
+                            : 'User not have post'
 
-                        this.state.userPost ?
-                        this.state.userPost.map(item => (
-                            <div key={item.id}>
-                                <UserPost usp={item} />
-                                </div>
-                            ))
-                        : 'User not have post'
-                    }
-                </div>
-            </div>
+                        }
+                    </div>
+                    
+            </Container>
         );
     }
 }
--- src/Components/Users/UserPost.js
+++ src/Components/Users/UserPost.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Card, Button, CardHeader, CardBody,
+    CardTitle, CardText } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class UserPost extends Component {
     render() {
@@ -6,13 +9,18 @@ class UserPost extends Component {
             <div>
                 {
                     this.props.usp ?
-                        <div>
-                            Id: {this.props.usp.id}
-                            <br/>
-                            Title: {this.props.usp.title}
-                            <br/>
-                            Description: {this.props.usp.body}
-                        </div>
+                        <Card>
+                            <CardHeader>Post ID: {this.props.usp.id}</CardHeader>
+                            <CardBody>
+                            <CardTitle>{this.props.usp.title.substring(0, 25)+'...'}</CardTitle>
+                            <CardText>{this.props.usp.body.substring(0,100)+'...'}</CardText>
+                            <Button className="viewProfile">
+                                <Link to={`/posts/${this.props.usp.id}`}>
+                                    Read More
+                                </Link>
+                            </Button>
+                            </CardBody>
+                        </Card>
                     : 'User not have Post'
                 }
             </div>
